9d64436ec4e64971c3a323344849a745edaacb7a
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2022-03-31 15:59:35 +0100

    events e navbar

---
+++ _site/assets/js/event.js
@@ -0,0 +1,14 @@
+$('.event-container').click(function() {
+    $(this).toggleClass("d-block");
+    $(this).find('.event-img').toggleClass("event-full");
+    $(this).find(".closed-event").toggle();
+    $(this).find(".open-event").toggle();
+});
+$('.event-container').hover(function() {
+    let days = $(this).data('days');
+    let newdays = days.slice(0, -1)
+    const daysArr = newdays.split(',');
+    $.each( daysArr, function(i, val) {
+        $("#" + val).toggleClass("day-active");
+    });
+});
